fb6009121cb1f6c88966d900b930653e6e8debac
Lucas Henrique <lucas.hcleonardo@gmail.com>
2024-06-06 14:35:13 -0300

    feat: initial configuration

---
+++ src/main/java/com/car_repair_shop/CarRepairShopApplication.java
@@ -0,0 +1,13 @@
+package com.car_repair_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRepairShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRepairShopApplication.class, args);
+	}
+
+}
