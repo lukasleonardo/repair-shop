98873f678175bf3a9c1d79ee655cd18ab6d677f8
Lucas Henrique <lucas.hcleonardo@gmail.com>
2024-06-10 14:18:26 -0300

    feat: global exception handler

--- src/main/java/com/car_repair_shop/handler/GlobalExceptionHandler.java
+++ src/main/java/com/car_repair_shop/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.car_repair_shop.handler;
 
-import com.car_repair_shop.exception.CarNotFoundException;
+import com.car_repair_shop.dtos.exceptionDTO.ExceptionDTO;
+import com.car_repair_shop.exception.BadRequestException;
+import com.car_repair_shop.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,13 +11,29 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(CarNotFoundException.class)
-    public ResponseEntity<String> handleCarNotFoundException(CarNotFoundException e) {
-        return ResponseEntity.notFound().build();
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionDTO> handleGeneralException(Exception e) {
+        ExceptionDTO err = new ExceptionDTO("Erro interno do Servidor",HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new ResponseEntity<>(err,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ExceptionDTO> handleBadRequest(BadRequestException e) {
+        ExceptionDTO errorResponse = new ExceptionDTO(
+                e.getMessage(),
+                HttpStatus.BAD_REQUEST.value()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ExceptionDTO> handleCarNotFoundException(NotFoundException e) {
+        ExceptionDTO errorResponse = new ExceptionDTO(
+                e.getMessage(),
+                HttpStatus.NOT_FOUND.value()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+
 }
